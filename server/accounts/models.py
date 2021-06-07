172ce69acec211efeb9308df23fb4247bba49ecb
uuljan <u.aitnazarova@gmail.com>
2021-06-07 15:21:25 +0600

    #11 Добавлено поле username, добавлены валидации, обновлена страница админки

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-# Create your models here.
+from django.core.validators import MinLengthValidator
 import uuid
 
 
 class Organization(models.Model):
     name = models.CharField(max_length=300, blank=False, null=False)
-    secondary_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    secondary_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
 
     class Meta:
         db_table = 'Organization'
@@ -15,9 +15,11 @@ class Organization(models.Model):
     def __str__(self):
         return self.name
 
+
 class Users(models.Model):
     tg_user = models.OneToOneField('kitchen5bot.TelegramUser', on_delete = models.CASCADE)
     organization_id = models.ForeignKey('accounts.Organization', on_delete=models.CASCADE, null=True, blank=True)
+    username = models.CharField(max_length=200, null=False, blank=False, unique=True, validators=[MinLengthValidator(3)])
 
     class Meta:
         db_table = 'Users'
