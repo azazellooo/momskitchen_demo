711f730574f8ec172063eb65c142c7c992f573d1
Bars <barsbekclash@gmail.com>
2021-06-12 13:45:41 +0600

    #19 Ребут Миграции

--- server/kitchen5bot/urls.py
+++ server/kitchen5bot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import handle_bot_request, poll_updates
 
+app_name = 'kitchen5bot'
+
 urlpatterns = [
     path('update/', handle_bot_request),
     path('poll/', poll_updates)
