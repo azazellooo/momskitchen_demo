01521f3221cf24d4e49a6df2f956df35d7ce0b18
uuljan <u.aitnazarova@gmail.com>
2021-06-14 12:56:56 +0600

    #21 Обновлен основной urls.py, написан UserProfileView для просмотра профиля пользователя, сконфигурированы пути для просмотра профиля через токен.

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, TemplateView
 
-# Create your views here.
+from accounts.models import Users, UserToken
+
+
+class UserProfileView(TemplateView):
+    template_name = 'profile.html'
+    def get(self, request, **kwargs):
+        self.user = get_object_or_404(UserToken, key=kwargs['token'])
+        return super().get(request, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['user'] = self.user
+        return context
