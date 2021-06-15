0113ca7453aca702aaf51fd7e65c43887c1e4934
Gosha <begoshaaaa@gmail.com>
2021-06-15 15:58:09 +0600

    "#41 создана модель категории "

--- server/KitchenWeb/models.py
+++ server/KitchenWeb/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.TextField(max_length=30, null=False, blank=False, verbose_name='Category')
+    order = models.IntegerField(null=False, blank=False, verbose_name='Category Order', validators=(MinValueValidator(1),))
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'Catgory: {self.category_name}, order: {self.order}'
