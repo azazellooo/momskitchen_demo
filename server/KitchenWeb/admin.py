c3049ec4267c3d08759b4dcaf6c20dfb5caf4216
Gosha <begoshaaaa@gmail.com>
2021-06-15 16:02:41 +0600

    "#41 модель категории зарегистрированра в admin.py "

--- server/KitchenWeb/admin.py
+++ server/KitchenWeb/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name', 'order']
+    list_filter = ['category_name']
+    search_fields = ['category_name']
+    fields = ['id', 'category_name', 'order']
+    readonly_fields = ['id']
+
+admin.site.register(Category, CategoryAdmin)
