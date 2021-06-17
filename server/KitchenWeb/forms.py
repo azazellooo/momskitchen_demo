52faf79a7bc66f38e9b1662f5f9d833fbae79ef4
insan <insanboss@gmail.com>
2021-06-17 15:21:39 +0600

    #46 * Реализовано создание надбавки

--- server/KitchenWeb/forms.py
+++ server/KitchenWeb/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from KitchenWeb.models import Supplement
+
 
 class SearchForm(forms.Form):
     search_value = forms.CharField(max_length=100, required=False, label='Найти')
+
+
+class SupplementForm(forms.ModelForm):
+    class Meta:
+        model = Supplement
+        fields = ['name', 'price']
--- server/KitchenWeb/views/kitchen.py
+++ server/KitchenWeb/views/kitchen.py
@@ -1,8 +1,10 @@
 from django.db.models import Q
 from django.shortcuts import render
+from django.urls import reverse
+
 from KitchenWeb.models import Supplement
-from django.views.generic import ListView
-from KitchenWeb.forms import SearchForm
+from django.views.generic import ListView, CreateView
+from KitchenWeb.forms import SearchForm, SupplementForm
 from django.utils.http import urlencode
 
 
@@ -39,3 +41,12 @@ class SupplementListView(ListView):
         if self.search_data:
             context['query'] = urlencode({'search_value': self.search_data})
         return context
+
+
+class SupplementCreateView(CreateView):
+    template_name = 'supplements/create.html'
+    model = Supplement
+    form_class = SupplementForm
+
+    def get_success_url(self):
+        return reverse('kitchen:supplement-list')
