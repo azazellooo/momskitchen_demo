b7c7b7c142b5ce334674d8592e1f46c514b4fd4c
insan <insanboss@gmail.com>
2021-06-17 15:24:15 +0600

    #46 *Добавлена ссылка на переход создания надбавки в навбаре * Убран permission мешающий для отображения ссылок в навбаре * добавлен урл роут

--- server/KitchenWeb/urls.py
+++ server/KitchenWeb/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from KitchenWeb.views.organizations import (
     OrganizationsListView
 )
-from KitchenWeb.views.kitchen import SupplementListView
+from KitchenWeb.views.kitchen import (
+    SupplementListView,
+    SupplementCreateView,
+)
 
 app_name = 'kitchen'
 
@@ -11,7 +14,8 @@ organization_urls = [
 ]
 
 kitchen_urls = [
-    path('supplements/', SupplementListView.as_view(), name='supplement-list')
+    path('supplements/', SupplementListView.as_view(), name='supplement-list'),
+    path('supplements/create/', SupplementCreateView.as_view(), name='create_supplement'),
 ]
 
 urlpatterns = [
