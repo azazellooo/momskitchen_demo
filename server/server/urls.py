48728a3ee50394513bed5f111210eca362f4c17f
Gosha <begoshaaaa@gmail.com>
2021-06-19 15:35:43 +0600

    "#50 добавлен media root для работы с файлами"

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from server import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('api/', include('api.urls')),
     path('', include('KitchenWeb.urls'))
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
