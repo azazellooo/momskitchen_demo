b903bce4493faa1507e2c5c521474672f46ea604
alishka1216 <alishka1216@gmail.com>
2021-06-21 17:47:26 +0600

    #56 добавил local_settings.py для соединения с базой данных добавил settings.py базу данных

---
+++ server/server/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bot_kitchen',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+    }
+}
