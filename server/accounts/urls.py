b9bf600e826ad0e6196116d6482f9b3e4cb512f0
Bars <barsbekclash@gmail.com>
2021-06-22 15:56:12 +0600

    №42 Создание Сессии, и асинхронного таска Селери который удаляет сессию и обьект UserTokenа спустя 30 сек (тестовое время в дальнейшем поменяем на 36 часов)

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -5,7 +5,8 @@ from accounts.views import UserProfileView, UserUpdateView
 
 urlpatterns = [
     path('<uuid:token>/', UserProfileView.as_view(), name='profile'),
-    path('update/<uuid:token>/', UserUpdateView.as_view(), name='user_update'),
+    path('', UserProfileView.as_view(), name='profile_distoken'),
+    path('update/', UserUpdateView.as_view(), name='user_update')
 ]
 
 
