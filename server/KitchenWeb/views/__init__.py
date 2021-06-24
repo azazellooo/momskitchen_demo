e049342e76e2162868fa58766b81482294fdae47
uuljan <u.aitnazarova@gmail.com>
2021-06-24 18:12:10 +0600

    #65_Реализован GarnishCreateView, сконфигурированы пути для создания гарнира

--- server/KitchenWeb/views/__init__.py
+++ server/KitchenWeb/views/__init__.py
@@ -7,4 +7,6 @@ from KitchenWeb.views.kitchen import SupplementListView
 
 from KitchenWeb.views.position import PositionCreateView
 
+from KitchenWeb.views.garnish_create import GarnishCreateView
+
 from KitchenWeb.views.category import CategoryListView
