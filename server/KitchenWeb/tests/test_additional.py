cd4e02190ca73ebfc41745165eda4cd6f7dda081
alishka1216 <alishka1216@gmail.com>
2021-06-25 17:16:51 +0600

    #72 создал list view для дополнений создал тест на list view для дополнений создал шаблон для списка просмотра дополнений к позиции

---
+++ server/KitchenWeb/tests/test_additional.py
@@ -0,0 +1,34 @@
+from time import sleep
+import requests
+
+from django.test import TestCase, RequestFactory, LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+from selenium.webdriver import Chrome
+
+from KitchenWeb.views import OrganizationCreateView
+from accounts.models import Organization
+
+
+class OrganizationsListViewTests(TestCase):
+    fixtures = ['additional_test_data.json']
+    response = None
+
+    def setUp(self):
+        self.response = self.client.get(reverse('kitchen:additional_list'))
+
+    def test_status_code_200(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_no_organization_response(self):
+        if len(self.response.context['additionals']) <= 0:
+            self.assertContains(self.response, 'Нет организаций')
+
+    def test_valid_response_for_search_query(self):
+        search_field_inner = 'a'
+        search_response = self.client.get('/additional/', {'search_value': search_field_inner})
+        [self.assertIn(search_field_inner, organization.name) for organization in
+         search_response.context['additionals']]
+
+    def test_is_paginated_by_5(self):
+        self.assertLessEqual(len(self.response.context['additional']), 5)
