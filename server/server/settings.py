f0cd0a262420042ef8b29b0a2d379d373a5cb8d7
Gosha <begoshaaaa@gmail.com>
2021-06-29 15:51:06 +0600

    #70 переход с локал сеттингс на decouple environ

--- server/server/settings.py
+++ server/server/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'accounts.middlewares.auth.CustomAuthMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
