db0808db1fbd58bd0f1e9dd56ba4d880f0d99fe5
karlygach <karygachkenenbaeva@gmail.com>
2021-06-30 15:19:02 +0600

    #69 написаны тесты на GarnishDetailUpdateView

--- server/KitchenWeb/tests/test_garnishes.py
+++ server/KitchenWeb/tests/test_garnishes.py
@@ -1,12 +1,17 @@
 import json
+import requests
+from time import sleep
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.test.utils import override_settings
+
+from selenium.webdriver import Chrome
 
 from KitchenWeb.models import Garnish
 from KitchenWeb.views import GarnishCreateView
 from accounts.models import Organization
-
 
 
 class GarnishListViewTests(TestCase):
@@ -33,7 +38,6 @@ class GarnishListViewTests(TestCase):
         self.assertLessEqual(len(self.response.context['garnishes']), 5)
 
 
-
 class GarnishCreateViewTests(TestCase):
 
     def setUp(self):
@@ -48,7 +52,6 @@ class GarnishCreateViewTests(TestCase):
         self.response = GarnishCreateView.as_view()(self.request)
         self.factory = RequestFactory()
         self.garnish = Garnish.objects.create(**self.data)
-
 
     def test_proper_template(self):
         self.assertTemplateUsed("garnishes/create.html")
@@ -66,10 +69,95 @@ class GarnishCreateViewTests(TestCase):
     def test_create(self):
         self.assertTrue(Garnish.objects.filter(name='garnish').exists())
 
-
     def test_field_values(self):
         self.assertEqual('garnish', self.garnish.name)
         self.assertEqual('1', self.garnish.order)
         self.assertEqual(100, self.garnish.base_price)
         self.assertEqual(json.loads('{"0.5": {"comment": "Comment", "pricing": "25"}}'), self.garnish.extra_price)
 
+
+@override_settings(DEBUG=True)
+class GarnishDetailUpdateViewTests(StaticLiveServerTestCase):
+    def setUp(self):
+        json_field = "{\"0.3\": {\"comment\": \"Comment3\", \"pricing\": \"20\"}, \"0.5\": {\"comment\": \"Comment22222\", \"pricing\": \"20\"}}"
+        self.garnish = Garnish.objects.create(**{
+            "name": "test garnish",
+            "order": "1",
+            "base_price": 100,
+            "extra_price": json_field
+        })
+        self.driver = Chrome()
+
+    def tearDown(self):
+        self.garnish.delete()
+        self.driver.close()
+
+    def test_use_proper_html(self):
+        response = self.client.get(f'{self.live_server_url}/kitchen/garnish/{self.garnish.pk}/')
+        self.assertTemplateUsed(response, 'garnishes/detail_update.html')
+
+    def test_disabled_enabled_fields(self):
+        self.driver.get(url=f'{self.live_server_url}/kitchen/garnish/{self.garnish.pk}/')
+        inputs = self.driver.find_elements_by_tag_name('input')
+        selects = self.driver.find_elements_by_tag_name('select')
+        buttons = self.driver.find_elements_by_class_name('plus-min-btn')
+        checking_elems = [y for x in [inputs, selects, buttons] for y in x]
+        edit_btn = self.driver.find_element_by_id('edit_btn')
+        cancel_btn = self.driver.find_element_by_id('cancel_btn')
+
+        self.assertTrue(edit_btn.is_displayed())
+        self.assertFalse(cancel_btn.is_displayed())
+        for i in checking_elems:
+            self.assertFalse(i.is_enabled())
+        edit_btn.click()
+        self.assertTrue(cancel_btn.is_displayed())
+        self.assertFalse(edit_btn.is_displayed())
+        for i in checking_elems:
+            self.assertTrue(i.is_enabled())
+        cancel_btn.click()
+        for i in checking_elems:
+            self.assertFalse(i.is_enabled())
+
+    def test_update(self):
+        self.driver.get(url=f'{self.live_server_url}/kitchen/garnish/{self.garnish.pk}/')
+        self.driver.find_element_by_id('edit_btn').click()
+        self.driver.find_element_by_name('name').clear()
+        self.driver.find_element_by_name('name').send_keys('updated garnish name')
+        self.driver.find_element_by_name('order').clear()
+        self.driver.find_element_by_name('order').send_keys('3')
+
+        self.driver.find_element_by_id('comment1').clear()
+        self.driver.find_element_by_name('comment1').send_keys('updated comment 1')
+
+        self.driver.find_element_by_id('pricing1').clear()
+        self.driver.find_element_by_name('pricing1').send_keys('123')
+
+        self.driver.find_element_by_id('comment2').clear()
+        self.driver.find_element_by_name('comment2').send_keys('updated comment 2')
+
+        self.driver.find_element_by_id('pricing2').clear()
+        self.driver.find_element_by_name('pricing2').send_keys('123')
+        self.driver.find_element_by_id('savePosition').click()
+        self.garnish.refresh_from_db()
+
+        self.assertEqual('updated garnish name', self.garnish.name)
+        self.assertEqual(3, self.garnish.order)
+
+        extra_dict = json.loads(self.garnish.extra_price)
+
+        self.assertEqual('updated comment 1', extra_dict['0.3']['comment'])
+        self.assertEqual('updated comment 2', extra_dict['0.5']['comment'])
+        self.assertEqual('123', extra_dict['0.3']['pricing'])
+        self.assertEqual('123', extra_dict['0.5']['pricing'])
+
+        self.assertEqual(f'{self.live_server_url}/kitchen/garnish/list/', self.driver.current_url)
+        response = requests.get(self.driver.current_url)
+        self.assertEqual(200, response.status_code)
+
+    def test_delete_extra_price(self):
+        self.driver.get(url=f'{self.live_server_url}/kitchen/garnish/{self.garnish.pk}/')
+        self.driver.find_element_by_id('edit_btn').click()
+        self.driver.find_elements_by_xpath("//button[@onclick='removeExtraBtn2(event);']")[0].click()
+        self.driver.find_element_by_id('savePosition').click()
+        self.garnish.refresh_from_db()
+        self.assertFalse('0.3' in self.garnish.extra_price)
