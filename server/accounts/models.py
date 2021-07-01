8ecacbafd867e322eae790eb8f0094a8c8d31e5f
Bars <barsbekclash@gmail.com>
2021-07-01 23:24:15 +0600

    #87 Изменение имени модели Users на Employe

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -31,7 +31,7 @@ class Organization(models.Model):
         return self.name
 
 
-class Users(models.Model):
+class Employe(models.Model):
     tg_user = models.OneToOneField('kitchen5bot.TelegramUser',
                                    on_delete=models.CASCADE, verbose_name='Пользователь телеграма')
     organization_id = models.ForeignKey('accounts.Organization', on_delete=models.CASCADE,
@@ -48,7 +48,7 @@ class Users(models.Model):
 
 class UserToken(models.Model):
     key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
-    user = models.OneToOneField('accounts.Users', on_delete=models.CASCADE, related_name='user_token')
+    user = models.OneToOneField('accounts.Employe', on_delete=models.CASCADE, related_name='user_token')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
