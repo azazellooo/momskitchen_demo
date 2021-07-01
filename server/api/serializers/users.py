fad38e17396c275bb3b2c5e59554017796bdd3ad
Bars <barsbekclash@gmail.com>
2021-07-01 23:27:27 +0600

    #87 рефакторинг имени в использовании вьюшки и импортов

--- server/api/serializers/users.py
+++ server/api/serializers/users.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from accounts.models import Users
+from accounts.models import Employe
 
 
 class UsersSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Users
+        model = Employe
         fields = ['id', 'tg_user', 'organization_id', 'username', 'is_active']
         read_only_fields = ['tg_user', 'organization_id']
 
