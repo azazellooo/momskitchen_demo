db4b48f7a837c3766d5c6e24a2b4a9aef2f07301
Bars <barsbekclash@gmail.com>
2021-07-01 23:03:26 +0600

    #85 добавление поля Activated в модели UserToken и на ходу удалил celery кэш, а так же реализовал во вьюшке авторизации одноразовый переход

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -49,6 +49,7 @@ class Users(models.Model):
 class UserToken(models.Model):
     key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
     user = models.OneToOneField('accounts.Users', on_delete=models.CASCADE, related_name='user_token')
+    activated = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
