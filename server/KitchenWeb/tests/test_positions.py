eca63fbea51c0f932d0e092ea8fc3f570f8da124
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-07-21 18:20:16 +0600

    #55 Написал тест на детальное обновление позиций

--- server/KitchenWeb/tests/test_positions.py
+++ server/KitchenWeb/tests/test_positions.py
@@ -1,8 +1,10 @@
+import json
+
 from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 from KitchenWeb.tests.factory_boy import OrganizationFactory, EmployeeFactory, UserTokenFactory, DishFactory, \
     CategoryFactory
-
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class PositionListViewTests(TestCase):
     fixtures = ['positions_test_data.json']
@@ -31,3 +33,41 @@ class PositionListViewTests(TestCase):
 
     def test_is_paginated_by_5(self):
         self.assertGreaterEqual(5, len(self.response.context['dishes']))
+
+
+class PositionDetailUpdateViewTests(TestCase):
+
+    def setUp(self):
+        self.organization = OrganizationFactory()
+        self.employee = EmployeeFactory(organization_id=self.organization)
+        self.token = UserTokenFactory(user=self.employee)
+        self.client.get(reverse('profile', kwargs={'token': self.token.key}))
+        self.category = CategoryFactory()
+        self.position = DishFactory(category=self.category)
+        self.response = self.client.get(reverse('kitchen:detail_update_position', kwargs={'pk': self.position.pk}))
+
+    def test_status_200(self):
+        self.assertEqual(200, self.response.status_code)
+        print(self.response.status_code)
+
+    def test_update_position(self):
+        new_jsons = {"0.5": {"comment": "Comment new", "pricing": "30"}}
+        new_json = json.dumps(new_jsons)
+        data_to_update = {
+            'name': DishFactory(name='another test dish name').name,
+            'description': 'another test dish description',
+            'category': CategoryFactory(category_name='another category').pk,
+            'base_price': 214,
+            'extra_price': new_json
+        }
+        response = self.client.post(reverse('kitchen:detail_update_position', kwargs={'pk': self.position.pk}),
+                                    data=data_to_update)
+
+        self.assertEqual(302, response.status_code)
+        self.assertRedirects(response, reverse("kitchen:list_position"))
+        self.position.refresh_from_db()
+        self.assertEqual('another test dish name', self.position.name)
+        self.assertEqual('another test dish description', self.position.description)
+        self.assertEqual('another category', self.position.category.category_name)
+        self.assertEqual(214, self.position.base_price)
+        # self.assertEqual(new_json, self.position.extra_price)
