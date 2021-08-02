2fa2f638d001fb1cbc2e92a2ac9f95666e6544c6
alishka1216 <alishka1216@gmail.com>
2021-08-02 15:09:25 +0600

    #125 удаление processors.py #125 удаление urls.py #125 удаление views.py

--- server/kitchen5bot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import handle_bot_request, poll_updates
-
-app_name = 'kitchen5bot'
-
-urlpatterns = [
-    path('update/', handle_bot_request),
-    path('poll/', poll_updates)
-]
