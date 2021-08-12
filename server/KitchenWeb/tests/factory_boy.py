9b1e2f6a67f13a340dad34aafcf20f0e40696f19
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-08-12 14:36:15 +0600

    #123 Отрефакторил тест на (additional_update).Исправил в (factory_boy) ввод json обьектов.Добавил в бот (message).

--- server/KitchenWeb/tests/factory_boy.py
+++ server/KitchenWeb/tests/factory_boy.py
@@ -1,3 +1,5 @@
+import json
+
 import factory
 from KitchenWeb.models import Category, Offering, Dish, Garnish, Additional
 import random
@@ -5,40 +7,18 @@ from accounts.models import Organization, Employee, UserToken, BalanceChange
 from KitchenWeb.models import Supplement
 
 
-
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
+
     category_name = 'test category'
     order = 3
 
 
-# class TelegramUserFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = TelegramUser
-#     telegram_id = random.randint(900000000, 941151633)
-#     is_bot = False
-#     first_name = 'Test'
-#     last_name = 'TelegramUser'
-#     username = 'test_tg_user'
-
-
-# class TelegramChatFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = TelegramChat
-#
-#
-# class TelegramStateFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = TelegramState
-#
-#     telegram_user = factory.SubFactory(TelegramUserFactory)
-#     telegram_chat = factory.SubFactory(TelegramChatFactory)
-#
-
 class OrganizationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Organization
+
     name = "Test Organization"
     payment = "('actual', 'фактический расчет')"
     address = "Bishkek"
@@ -57,13 +37,13 @@ class EmployeeFactory(factory.django.DjangoModelFactory):
     username = factory.Faker('name')
     is_active = True
     is_admin = True
-    # bal_em = factory.RelatedFactoryList(BalanceChangeFactory, size=3)
     total_balance = 10
 
 
 class BalanceChangeFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = BalanceChange
+
     employee = factory.SubFactory(EmployeeFactory)
     type = 'accrual'
     sum_balance = 12
@@ -88,27 +68,32 @@ class SupplementFactory(factory.django.DjangoModelFactory):
 class DishFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Dish
+
     name = 'test position'
     description = 'test description'
     category = factory.SubFactory(CategoryFactory)
     base_price = 123
-    extra_price = '{"0.5": {"comment": "Comment", "pricing": "25"}}'
+    extra_price = json.dumps({"0.5": {"comment": "Comment", "pricing": "230"}})
 
 
 class GarnishFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Garnish
+
     name = 'test garnish'
     order = 2
     base_price = 13
+    extra_price = json.dumps({"0.5": {"comment": "Comment", "pricing": "230"}})
 
 
 class AdditionalFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Additional
+
     name = 'test additional'
     sampling_order = 2
     base_price = 123
+    extra_price = json.dumps({"0.5": {"comment": "Comment", "pricing": "230"}})
 
 
 class OfferingFactory(factory.django.DjangoModelFactory):
