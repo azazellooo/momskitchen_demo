bf97bce35db95400b434adc32c77b5af1fe6f6ab
karlygach <karygachkenenbaeva@gmail.com>
2021-08-14 18:43:55 +0600

    #134 добавлен еще один url (без слага) на вьюшку UserProfileView

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -4,6 +4,7 @@ from accounts.views import UserProfileView, UserUpdateView, EmployeeTransactionH
 
 urlpatterns = [
     path('<uuid:token>/<slug:slug>', UserProfileView.as_view(), name='profile'),
+    path('<uuid:token>/', UserProfileView.as_view(), name='profile'),
     path('', UserProfileView.as_view(), name='profile_distoken'),
     path('update/', UserUpdateView.as_view(), name='user_update'),
     path('<int:pk>/transactions/', EmployeeTransactionHistoryView.as_view(), name='employee-transactions'),
