75f5f30d6815965f12c4a69b7f0954d5b195a8d0
Gosha <begoshaaaa@gmail.com>
2021-08-24 15:26:24 +0600

    добавили placeholder

--- server/KitchenWeb/static/js/kitchen.js
+++ server/KitchenWeb/static/js/kitchen.js
@@ -6,9 +6,9 @@ function addExtra(event){
     TYPES = JSON.parse(document.getElementById('types').textContent);
     add.style.visibility = 'hidden'
 	$("<div/>").attr({id:'selectBlock'}).appendTo(block)
-	$("<input/>").attr({type: 'text', id:'comment', value:
+	$("<input/>").attr({type: 'text', id:'comment', placeholder:
         'Комментарий', class:'form-control w-75 p-3', name:('comment' + TYPES.length)}).appendTo('#selectBlock');
-    $("<input/>").attr({type: 'number', id:'pricing', value:
+    $("<input/>").attr({type: 'number', id:'pricing', placeholder:
         '0', class:'form-control mt-4 w-75 p-3', name:('pricing' + TYPES.length)}).appendTo('#selectBlock');
     $("<select/>").attr({id:TYPES.length, name: TYPES.length}).appendTo('#selectBlock');
     let select = document.getElementById(TYPES.length)
@@ -35,9 +35,9 @@ function addExtra(event){
 		 let toRemove = TYPES.indexOf(parseFloat(selected))
 		 TYPES.splice(toRemove, 1)
 		 $("<div/>").attr({id:'selectBlock1'}).appendTo(block)
-		 $("<input/>").attr({type: 'text', id:'comment1', class:'form-control mt-4 w-75 p-3', value:
+		 $("<input/>").attr({type: 'text', id:'comment1', class:'form-control mt-4 w-75 p-3', placeholder:
         'Комментарий', name:('comment' + TYPES.length)}).appendTo('#selectBlock1');
-    	 $("<input/>").attr({type: 'number', id:'pricing1', class:'form-control mt-4 w-75 p-3', value:
+    	 $("<input/>").attr({type: 'number', id:'pricing1', class:'form-control mt-4 w-75 p-3', placeholder:
         '0', name:('pricing' + TYPES.length)}).appendTo('#selectBlock1');
     	 $("<select/>").attr({id:TYPES.length, name: TYPES.length}).appendTo('#selectBlock1');
 		 let select1 = document.getElementById(TYPES.length)
